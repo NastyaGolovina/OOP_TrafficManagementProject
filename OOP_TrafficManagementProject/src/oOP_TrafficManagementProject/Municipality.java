a4a53c4f32a14264769df911e0a560e0c84bab7d
macbook <macbook@localhost>
2024-05-02 11:54:06 +0100

    Create Municipality class Instances Variable, Constructor and Getter method

--- OOP_TrafficManagementProject/src/oOP_TrafficManagementProject/Municipality.java
+++ OOP_TrafficManagementProject/src/oOP_TrafficManagementProject/Municipality.java
@@ -1,9 +1,60 @@
 package oOP_TrafficManagementProject;
 
+import java.util.ArrayList;
+
 public class Municipality {
+	private String name;
+	private double maxSpeed;
+	private double averageDistanceBetweenCars;
+	private ArrayList<Vehicle> VehicleList ; // VehicleList
+	private ArrayList<Road> RoadList ; // RoadList
+	private ArrayList<CrossRoad> CrossRoadList; // CrossRoadList
+	
 
-	public Municipality() {
+	public Municipality (String name, double maxSpeed, double averageDistanceBetweenCars) {
 		// TODO Auto-generated constructor stub
+      this.name = name;
+      this.maxSpeed = maxSpeed;
+      this.averageDistanceBetweenCars = averageDistanceBetweenCars;
+      VehicleList = new ArrayList<Vehicle>();
+      RoadList = new ArrayList<Road>();
+      CrossRoadList = new ArrayList<CrossRoad>();
+      
 	}
-
+  // Get method for all instances variables and array
+	public String getName() {
+		return name;
+	}
+	
+	public double getMaxSpeed() {
+		return maxSpeed;
+	}
+	
+	public double getAverageDistanceBetweenCars() {
+		return averageDistanceBetweenCars;
+	}
+	
+	public ArrayList<Vehicle> getVehicleList() {
+		return VehicleList;
+		
+	}
+	public ArrayList<Road> getRoadList() {
+		return RoadList;
+	}
+	
+	public ArrayList<CrossRoad> getCrossRoadList() {
+		return CrossRoadList;
+		
+	}
+	// Setter method 
+	
+	public static void setName()
+	
+	
+	
+	
+	
+	
+	
+	
 }
