5d2fe2a74652ea8312d0300a6c1dd6fba8c376a7
NastyaGolovina <100077963+NastyaGolovina@users.noreply.github.com>
2024-05-03 12:09:51 +0300

    change vehicle

--- OOP_TrafficManagementProject/src/oOP_TrafficManagementProject/Vehicle.java
+++ OOP_TrafficManagementProject/src/oOP_TrafficManagementProject/Vehicle.java
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
 package oOP_TrafficManagementProject;
 
 public class Vehicle {
-
 	public Vehicle() {
 		// TODO Auto-generated constructor stub
 	}
 
 }
-=======
-package oOP_TrafficManagementProject;
-
-public class Vehicle {
-
-	public Vehicle() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/NastyaGolovina/OOP_TrafficManagementProject.git
