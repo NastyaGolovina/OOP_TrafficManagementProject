a4becfff138227f4695ecbb28219cca5dd9ac244
NastyaGolovina <100077963+NastyaGolovina@users.noreply.github.com>
2024-05-04 12:55:05 +0300

    Update TrafficLight class

--- OOP_TrafficManagementProject/src/oOP_TrafficManagementProject/TrafficLight.java
+++ OOP_TrafficManagementProject/src/oOP_TrafficManagementProject/TrafficLight.java
@@ -1,9 +1,39 @@
 package oOP_TrafficManagementProject;
 
 public class TrafficLight {
+	/**
+	*  instance variables of class
+	*/
+	private int trafficLightId;
+	private String type;
 
-	public TrafficLight() {
-		// TODO Auto-generated constructor stub
+	 /**
+	 * Constructor for objects of class TrafficLight
+	 */
+	public TrafficLight(int trafficLightId , String type)
+	{
+	this.trafficLightId = trafficLightId;
+	this.type = type;
 	}
-
+	/**
+	 * getter method 
+	 */
+	public int getTrafficLightId () {
+	  return trafficLightId;
+	}
+	
+	public String getType () {
+	  return type;
+	}
+	
+	/**
+	 *  Setter method
+	 */
+	public void setTrafficLightId (int trafficLightId) {
+	this.trafficLightId = trafficLightId;
+	}
+	
+	public void setType (String type){
+	 this.type = type;
+	}
 }
