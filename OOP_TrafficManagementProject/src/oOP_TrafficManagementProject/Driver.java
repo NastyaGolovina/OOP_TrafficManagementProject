aa2d3c6eb0d1492d09ead2215f64de8672589251
NastyaGolovina <100077963+NastyaGolovina@users.noreply.github.com>
2024-05-14 14:28:02 +0300

    Add Driver and Vehicle classes

--- OOP_TrafficManagementProject/src/oOP_TrafficManagementProject/Driver.java
+++ OOP_TrafficManagementProject/src/oOP_TrafficManagementProject/Driver.java
@@ -1,5 +1,110 @@
 package oOP_TrafficManagementProject;
 
 public class Driver {
-	private String driverID;
+	//Initialization 
+	private String driverId;
+	private String driverName;
+	private String driverLicense;
+	private String email;
+	private String phone; 
+	
+
+	public Driver(String driverId, String driverName, String driverLicense, String email, String phone) {
+        this.driverId = driverId;
+        this.driverName = driverName;
+        this.driverLicense = driverLicense;
+        this.email = email;
+        this.phone = phone;
+    }
+
+
+	/**
+	 * @return the driverId
+	 */
+	public String getDriverId() {
+		return driverId;
+	}
+
+
+	/**
+	 * @param driverId the driverId to set
+	 */
+	public void setDriverId(String driverId) {
+		this.driverId = driverId;
+	}
+
+
+	/**
+	 * @return the driverName
+	 */
+	public String getDriverName() {
+		return driverName;
+	}
+
+
+	/**
+	 * @param driverName the driverName to set
+	 */
+	public void setDriverName(String driverName) {
+		this.driverName = driverName;
+	}
+
+
+	/**
+	 * @return the driverLicense
+	 */
+	public String getDriverLicense() {
+		return driverLicense;
+	}
+
+
+	/**
+	 * @param driverLicense the driverLicense to set
+	 */
+	public void setDriverLicense(String driverLicense) {
+		this.driverLicense = driverLicense;
+	}
+
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+
+
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	@Override
+	public String toString() {
+		return "driverId=" + driverId + ", driverName=" + driverName + ", driverLicense=" + driverLicense
+				+ ", email=" + email + ", phone=" + phone;
+	}
+	
+	
+	
+	
 }
--- OOP_TrafficManagementProject/src/oOP_TrafficManagementProject/Vehicle.java
+++ OOP_TrafficManagementProject/src/oOP_TrafficManagementProject/Vehicle.java
@@ -1,6 +1,137 @@
 package oOP_TrafficManagementProject;
 
 public class Vehicle {
+	//	Instance variable
+	private String licensePlate;
+	private String carModel;
+	private double length;
+	private double width;
+	private double zeroTo100rate;
+	private Driver driver;
 	
+	public Vehicle(String licensePlate, String carModel, double length, double width, double zeroTo100Rate, Driver driver) {
+	    this.licensePlate = licensePlate;
+	    this.carModel = carModel;
+	    this.length = length;
+	    this.width = width;
+	    this.zeroTo100rate = zeroTo100Rate;
+	    this.driver = driver;
+	}
+	//Getters
+	
+	/**
+	 * get License plate
+	 * @return LicensePlate
+	 */
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+	
+	/**
+	 * get Car Model
+	 * @return CarModel
+	 */
+	public String getCarModel() {
+		return carModel;
+	}
+	
+	/**
+	 * get Length
+	 * @return length
+	 */
+	public double getLength() {
+		return length;
+	}
+	
+	/**
+	 * get Width
+	 * @return Width
+	 */
+	public double getWidth() {
+		return width;
+	}
+	
+	/**
+	 * get Zero to 100 rate
+	 * @return ZeroTo100rate
+	 */
+	public double getZeroTo100rate() {
+		return zeroTo100rate;
+	}
+	
+	/**
+	 * get Driver
+	 * @return driver
+	 */
+	public Driver getDriver() {
+		return driver;
+	}
+	
+	
+	//Setters
+	
+	/**
+	 * set license plate
+	 * @param licensePlate
+	 */
+	public void setLicensePlate(String licensePlate) {
+		this.licensePlate = licensePlate;
+	}
+	
+	/**
+	 * set Car Model
+	 * @param carModel
+	 */
+	public void setCarModel(String carModel) {
+		this.carModel = carModel;
+	}
+	
+	/**
+	 * set Length
+	 * @param length
+	 */
+	public void setLength(double length) {
+		this.length = length;
+	}
+	
+	/**
+	 * set Width
+	 * @param width
+	 */
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	
+	/**
+	 * set Zero to 100 rate
+	 * @param zeroTo100rate
+	 */
+	public void setZeroTo100rate(double zeroTo100rate) {
+		this.zeroTo100rate = zeroTo100rate;
+	}
+	
+	/**
+	 * set Driver
+	 * @param driver
+	 */
+	public void setDriver(Driver driver) {
+		this.driver = driver;
+	}
+	/**
+	 * accelerationCalc
+	 * @return acceleration
+	 */
+	public double accelerationCalc() {
+	    return 100/zeroTo100rate;
+	}
+
+	@Override
+	public String toString() {
+		return "licensePlate=" + licensePlate + ", carModel=" + carModel + ", length=" + length + ", width="
+				+ width + ", zeroTo100rate=" + zeroTo100rate + "/n driver=" + driver.toString();
+	}
+	
+	
+
 
 }
