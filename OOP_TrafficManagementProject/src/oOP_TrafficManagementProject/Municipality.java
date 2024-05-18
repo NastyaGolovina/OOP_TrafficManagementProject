4a238d6083e4e128ce84b4ef6e15a68c56b79dd8
NastyaGolovina <100077963+NastyaGolovina@users.noreply.github.com>
2024-05-18 18:51:59 +0300

    add comments in Municipality class

--- OOP_TrafficManagementProject/src/oOP_TrafficManagementProject/Municipality.java
+++ OOP_TrafficManagementProject/src/oOP_TrafficManagementProject/Municipality.java
@@ -25,152 +25,208 @@ public class Municipality {
       CrossRoadList = new ArrayList<CrossRoad>();
       
 	}
-  // Get method for all instances variables and array
+   // Get method for all instances variables and array
+	
+	
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
-	
+	/**
+	 * @return the maxSpeed
+	 */
 	public double getMaxSpeed() {
 		return maxSpeed;
 	}
-	
+	/**
+	 * @return the averageDistanceBetweenCars
+	 */
 	public double getAverageDistanceBetweenCars() {
 		return averageDistanceBetweenCars;
 	}
-	
+	/**
+	 * @return the vehicleList
+	 */
 	public ArrayList<Vehicle> getVehicleList() {
 		return VehicleList;
-		
 	}
+	/**
+	 * @return the roadList
+	 */
 	public ArrayList<Road> getRoadList() {
 		return RoadList;
 	}
-	
+	/**
+	 * @return the crossRoadList
+	 */
 	public ArrayList<CrossRoad> getCrossRoadList() {
 		return CrossRoadList;
-		
 	}
+
+
 	
 	// Setter method 
+	
+	 /**
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
 	/**
-	 * @param maxSpeed
+	 * @param maxSpeed the maxSpeed to set
 	 */
 	public void setMaxSpeed(double maxSpeed) {
 		this.maxSpeed = maxSpeed;
-		
 	}
-	
+
+
+	/**
+	 * @param averageDistanceBetweenCars the averageDistanceBetweenCars to set
+	 */
 	public void setAverageDistanceBetweenCars(double averageDistanceBetweenCars) {
 		this.averageDistanceBetweenCars = averageDistanceBetweenCars;
-		
 	}
+
+
+	/**
+	 * @param vehicleList the vehicleList to set
+	 */
 	public void setVehicleList(ArrayList<Vehicle> vehicleList) {
-        this.VehicleList = vehicleList;
-    }
-	
-	public void setRoadList(ArrayList<Road> RoadList) {
-		this.RoadList = RoadList;
+		VehicleList = vehicleList;
 	}
-	
-	public void setCrossRoadList(ArrayList<CrossRoad> CrossRoadList ) {
-		this.CrossRoadList = CrossRoadList;
+
+
+	/**
+	 * @param roadList the roadList to set
+	 */
+	public void setRoadList(ArrayList<Road> roadList) {
+		RoadList = roadList;
 	}
+
+
+	/**
+	 * @param crossRoadList the crossRoadList to set
+	 */
+	public void setCrossRoadList(ArrayList<CrossRoad> crossRoadList) {
+		CrossRoadList = crossRoadList;
+	}
+
 	
-	// add vehicle, road, Crossroad to list
 	
+	
+	
+	
+	/**
+	 * addVehicle
+	 * @param vehicle
+	 */
     public void addVehicle(Vehicle vehicle) {
     	VehicleList.add(vehicle);
     }
-	
-    public void addRoad(Road road ) {
+    /**
+     * addRoad
+     * @param road
+     */
+	public void addRoad(Road road ) {
     	RoadList.add(road);
     }
 	
+	/**
+     * addCrossRoad
+     * @param crossroad
+     */
 	public void addCrossRoad(CrossRoad crossroad) {
 		CrossRoadList.add(crossroad);
 	}
 	
-	// search vehicle, road, crossroad in list
-	
-	// search for vehicle by licensePlate
-	// return vehicle position if existed or -1 not existed
-	
-		
-		//search for driver
-		 public int searchVehicle(String licensePlate) {
-			 int i = 0;
-			   while (i< VehicleList.size() && !VehicleList.get(i).getLicensePlate().equalsIgnoreCase(licensePlate)) {
-				   i ++;
-			   }
-			   if (i != VehicleList.size() ) {
-				   return i; 
-			   }				 
-	           return -1;
-		 }
-		 
-	 /* *if (i != VehicleList.size()): This condition checks if the variable i is not equal to the size of the VehicleList. 
-	  * If i is equal to the size of VehicleList, it means the loop iterated through the entire list without finding a matching license plate.
-        return i;: 
-        If a matching license plate was found during the loop (i.e., i is not equal to the size of VehicleList), this line returns the index (i) where the matching license plate was found in the list.
-        return -1;
-        If no matching license plate was found in the list (i.e., i is equal to the size of VehicleList), this line returns -1 to indicate that no match was found.
-	   */
-		 
-		 
-		 // search Driver
-		 public int searchDriver(String driverID) {
-			    int i = 0;
-			    while (i < VehicleList.size() && !VehicleList.get(i).getDriver().getDriverId().equalsIgnoreCase(driverID)) {
-			        i++;
-			    }
-			    if (i != VehicleList.size()) {
-			        return i; // Driver found
-			    }
-			    return -1; // Driver not found
-			}
 
-		 // search Road
-		 public int searchRoad( String roadId) {
-			 int i = 0;
-			 while (i < RoadList.size() && !RoadList.get(i).getRoadID().equalsIgnoreCase(roadId)){
-				 i++;
-			 }
-			 if ( i != RoadList.size()) {
-				 return i;
-			 }
-			 return -1;
+
+	
+	/**
+	 * search for driver
+	 * @param licensePlate
+	 * @return pos
+	 */
+	 public int searchVehicle(String licensePlate) {
+		 int i = 0;
+		   while (i< VehicleList.size() && !VehicleList.get(i).getLicensePlate().equalsIgnoreCase(licensePlate)) {
+			   i ++;
+		   }
+		   if (i != VehicleList.size() ) {
+			   return i; 
+		   }				 
+           return -1;
+	 }
+
+	 
+	 /**
+	  * search Driver
+	  * @param driverID
+	  * @return   driverID pos
+	  */
+	 public int searchDriver(String driverID) {
+		    int i = 0;
+		    while (i < VehicleList.size() && !VehicleList.get(i).getDriver().getDriverId().equalsIgnoreCase(driverID)) {
+		        i++;
+		    }
+		    if (i != VehicleList.size()) {
+		        return i; // Driver found
+		    }
+		    return -1; // Driver not found
+		}
+
+	 /**
+	  * search Road
+	  * @param roadId
+	  * @return  roadId pos
+	  */
+	 public int searchRoad( String roadId) {
+		 int i = 0;
+		 while (i < RoadList.size() && !RoadList.get(i).getRoadID().equalsIgnoreCase(roadId)){
+			 i++;
 		 }
-	           
-		//searchCrossRoad
-		 public int searchCrossRoad( String crossRoadId) {
-			 int i = 0;
-			 while (i < CrossRoadList.size() && !CrossRoadList.get(i).getCrossRoadId().equalsIgnoreCase(crossRoadId)){
-				 i++;
-			 }
-			 if ( i != CrossRoadList.size()) {
-				 return i;
-			 }
-			 return -1;
-		 } 
-		 
-	     /**
-	      * searchTrafficLight 
-	      * @param trafficLightID
-	      * @return trafficLight pos
-	      */
-		 public int searchTrafficLight( String trafficLightID) {
-			 int i = 0;
-			 while (i < CrossRoadList.size() && !CrossRoadList.get(i).getHorisontalTrafficLight().getTrafficLightId().equalsIgnoreCase(trafficLightID)){
-				 i++;
-			 }
-			 if ( i != CrossRoadList.size()) {
-				 return i;
-			 }
-			 return -1;
-		 }       
+		 if ( i != RoadList.size()) {
+			 return i;
+		 }
+		 return -1;
+	 }
+           
+	/**
+	 * searchCrossRoad
+	 * @param crossRoadId
+	 * @return crossRoadId pos
+	 */
+	 public int searchCrossRoad( String crossRoadId) {
+		 int i = 0;
+		 while (i < CrossRoadList.size() && !CrossRoadList.get(i).getCrossRoadId().equalsIgnoreCase(crossRoadId)){
+			 i++;
+		 }
+		 if ( i != CrossRoadList.size()) {
+			 return i;
+		 }
+		 return -1;
+	 } 
+	 
+     /**
+      * searchTrafficLight 
+      * @param trafficLightID
+      * @return trafficLight pos
+      */
+	 public int searchTrafficLight( String trafficLightID) {
+		 int i = 0;
+		 while (i < CrossRoadList.size() && !CrossRoadList.get(i).getHorisontalTrafficLight().getTrafficLightId().equalsIgnoreCase(trafficLightID)){
+			 i++;
+		 }
+		 if ( i != CrossRoadList.size()) {
+			 return i;
+		 }
+		 return -1;
+	 }       
  
 	}
 	
